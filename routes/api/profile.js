212b476cd10274fb9f6b3b151911851b9bfe1722
Hassan Yusuf <hassanyousuf1999@gmail.com>
2022-11-23 11:33:43 -0500

    github client secret and id added and route set to retrieve github repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const request = require('request');
+const config = require('config');
 const { validationResult, check } = require('express-validator');
 
 const Profile = require('../../models/Profile');
@@ -306,6 +308,36 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     } catch (error) {
         console.log(error.message);
         res.status(500).send('Server error');   
+    }
+});
+
+// @route   Get api/profile/github/:username
+// @desc    Get Users github repos
+// @access  public
+router.get('/github/:username', async (req, res) => {
+    try {
+
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}
+            &client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers : { 'user-agent' : 'node.js' }
+        }
+
+        request(options, (error, response, body) => {
+            if(error) console.log(error);
+
+            if(response.statusCode !== 200) {
+                return res.status(404).json({ msg: 'No Github profile found' });
+            }
+
+            res.json(JSON.parse(body));
+        });
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Server error');
+        
     }
 })
 
