e793edcc5b0729093cdad12d37f0520b76f43dfd
Hassan Yusuf <hassanyousuf1999@gmail.com>
2022-11-23 14:58:36 -0500

    delete post by id route is set up

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -83,4 +83,32 @@ router.get('/:id', auth, async (req, res) => {
     }
 });
 
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post by ID
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        let post = await Post.findById(req.params.id);
+
+        if(!post) {
+            return res.status(400).json({ msg: 'No Posts found'});
+        }
+
+        if(post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized'});
+        }
+
+        await post.remove();
+        res.json({ msg: 'Post successfully deleted'});
+
+    } catch (error) {
+        console.log(error.message);
+        if(error.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'No Posts found'});
+        }
+        res.status(500).send('Server error');
+    }
+})
+
 module.exports = router;
