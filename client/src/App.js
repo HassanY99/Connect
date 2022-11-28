b87c1ae50e0c3fa127e0b2980cba2a6e00e031a4
Hassan Yusuf <hassanyousuf1999@gmail.com>
2022-11-28 13:47:07 -0500

    set auth token set which will be sent with every request so validate if user is logged in and load user is set

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Landing from "./components/Landing";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
@@ -7,8 +7,20 @@ import Register from "./components/Register";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
+import setAuthToken from "./utils/setAuthToken";
+import { loadUser } from "./actions/auth";
+
+
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 function App() {
+
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
 
     <div className="app">
