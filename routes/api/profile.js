9b3c65873bb66ae7a8106f1b1d1deb7813c13a3d
Hassan Yusuf <hassanyousuf1999@gmail.com>
2022-11-29 11:46:16 -0500

    profile reducer, get current user action set. Now logged in user profile is accessed in dashboard

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,27 +9,28 @@ const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
 
-// @route   Get api/profile/{user.id}
-// @desc    Get current user profile
-// @access  Private
-// router.get('/', auth, async (req, res) => {
-//     try {
-//         const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private
+router.get('/me', auth, async (req, res) => {
+    try {
+      const profile = await Profile.findOne({
+        user: req.user.id
+      }).populate('user', ['name', 'avatar']);
+  
+      if (!profile) {
+        return res.status(400).json({ msg: 'There is no profile for this user' });
+      }
+  
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
 
-//         if(!profile) {
-//             return res.status(400).json({ msg: 'There is no profile for this user.' });
-//         }
 
-//         res.json(profile);
-
-//     } catch (error) {
-//         console.log(error.message);
-//         res.status(500).send('Server error!');
-//     }
-// });
-
-
-// @route   Get api/profile/
+// @route   Get api/profile
 // @desc    Create or Update Profile
 // @access  Private
 router.post('/', auth, 
