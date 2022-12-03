36bc32677a4d27ac50603db949dee2a0ea39f9f7
Hassan Yusuf <hassanyousuf1999@gmail.com>
2022-12-03 14:40:57 -0500

    name changed inside prod and dev config

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,6 @@
 module.exports = {
     JWT_SECRET: "mysecretkey",
     MONGOURI : "mongodb+srv://ConnectApp:Connect321!@connect.6zwnkvp.mongodb.net/?retryWrites=true&w=majority",
+    GITHUBCLIENTID : "db9d9b5d694e84f6da1e",
+    GITHUBSECRET:"e7ea0677d4021f71d59b03048a92ffdd8712af58"
 }
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,6 @@
 module.exports = {
     JWT_SECRET: process.env.JWT_SECRET,
     MONGOURI : process.env.MONGOURI,
+    GITHUBCLIENTID : process.env.GITHUBCLIENTID,
+    GITHUBSECRET: process.env.GITHUBSECRET
 }
